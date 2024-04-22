73adae6e7658114ddc23a49ac6471cb0c10a6be2
DGOLD7788 <80936904545dgold@gmail.com>
2024-04-22 15:18:24 +0300

    Update task_two
    Update task_two

--- js/task_two.js
+++ js/task_two.js
@@ -3,6 +3,19 @@
 // Example 2 - Масиви та рядки
 // Напиши скрипт для обчислення площі прямокутника зі сторонами, значення яких зберігаються у змінній values у вигляді рядка. Значення гарантовано розділені пробілом.
 
-// const values = '8 11';
+const values = '8 11';
+
+let param = values.split(' ');
+console.log(param);
+
+param[0] = Number(param[0])
+param[1] = Number(param[1])
 
 
+function squ(a, b) {
+  return (a + b) * 2;
+}
+
+squ(param[0], param[1])
+
+console.log(squ(param[0], param[1]));
