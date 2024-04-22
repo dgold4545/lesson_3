a1edd730ca663f7425e4fa62f4c343f2ad648474
DGOLD7788 <80936904545dgold@gmail.com>
2024-04-22 15:35:37 +0300

    Update task_five

--- js/task_five.js
+++ js/task_five.js
@@ -2,3 +2,13 @@
 // Напиши скрипт який виводить у консоль усі слова рядка окрім першого та останнього. Результуючий рядок не повинен починатися або закінчуватися символ пробілу. Скрипт повинен працювати для будь-якого рядка.
 
 const string = 'Welcome to the future';
+
+const stringToArray = string.split(' ');
+
+
+const normalString = stringToArray.slice(1, -1)
+
+console.log(normalString);
+
+const result = normalString.join(' ')
+console.log(result);
