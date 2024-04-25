b79cac9e95bb3529c2991d885e479cd3068ad7f1
DGOLD7788 <80936904545dgold@gmail.com>
2024-04-25 09:28:49 +0300

    less297

---
+++ js_des/des.js
@@ -0,0 +1,17 @@
+'use strict';
+
+
+
+function func() {
+  return [2025, 12, 31];
+}
+
+const [year, month, day] = func()
+
+function func() {
+  return ['John', 'Smit', 'development', 'programmer', 2000];
+}
+
+const [name, surname, department, position, salary] = func()
+
+console.log(name.toUpperCase());
