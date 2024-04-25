64bd2f10736ae3098a597ec8a51a18a7d859cfb2
DGOLD7788 <80936904545dgold@gmail.com>
2024-04-25 10:10:20 +0300

    dest obj

--- js_des/des.js
+++ js_des/des.js
@@ -2,23 +2,23 @@
 
 
 
-function func() {
-  return [2025, 12, 31];
-}
-
-const [year, month, day] = func()
-
-function func() {
-  return ['John', 'Smit', 'development', 'programmer', 2000];
-}
-
-// const [name, surname, department, position, salary] = func()
-
-// console.log(name.toUpperCase());
+let obj = {
+  year: 2025,
+  month: 12,
+  day: 31,
+};
 
 
-let arr = ['John', 'Smit', 'development', 'programmer', 2000]; 
+const { year: y, month: m, day: d } = obj;
+console.log(y,d,m);
+let options = {
+  color: 'red',
+  width: 400,
+  height: 500,
+};
 
-const [, , department, position] = arr
 
-console.log(position, department);
+const { color: c, width: w, height: h } = options;
+
+
+console.log(c,h,w);
