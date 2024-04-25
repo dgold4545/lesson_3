623a33d2bed322d1154a310b4dc7042b02715dc9
DGOLD7788 <80936904545dgold@gmail.com>
2024-04-25 10:14:45 +0300

    dest fun

--- js_des/des.js
+++ js_des/des.js
@@ -2,23 +2,12 @@
 
 
 
-let obj = {
-  year: 2025,
-  month: 12,
-  day: 31,
-};
-
-
-const { year: y, month: m, day: d } = obj;
-console.log(y,d,m);
 let options = {
-  color: 'red',
   width: 400,
   height: 500,
+  color: 'green'
 };
 
+const { color: c = 'black', width, height } = options;
 
-const { color: c, width: w, height: h } = options;
-
-
-console.log(c,h,w);
+console.log(c);
