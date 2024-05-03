38406f66210a416198d9e514741f176cafd7a330
DGOLD7788 <80936904545dgold@gmail.com>
2024-05-03 16:19:42 +0300

    function fib

--- js_des/des.js
+++ js_des/des.js
@@ -84,3 +84,34 @@ function findMaxNumber(...rest) {
 }
 
 console.log(findMaxNumber(1, 5, 6.6, 11));
+
+/**
+ *  function fibчисла Фибоначчи
+ * @param {number} num 
+ * @returns {string}
+ */
+
+function fib(num) {
+  if (typeof num !== 'number' || num <= 0 || !Number.isInteger(num)) {
+    return '';
+  }
+
+  let result = '';
+  let first = 0;
+  let second = 1;
+
+  for (let i = 0; i < num; i++) {
+    if (i + 1 === num) {
+      result += `${first}`;
+      // Без пробела в конце
+    } else {
+      result += `${first} `;
+    }
+
+    let third = first + second;
+    first = second;
+    second = third;
+  }
+
+  return result;
+}
